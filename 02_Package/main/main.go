453785b6a7b63a190ddc4d45544c911fae6bb083
Corbin Estes <cwestes01@bvsd.org>
2019-05-24 14:45:22 -0600

    More things

---
+++ 02_Package/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/westes07/LearningGo/02_Package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
